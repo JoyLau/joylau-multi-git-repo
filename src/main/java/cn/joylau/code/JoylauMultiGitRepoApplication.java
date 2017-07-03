65f6f31aac938d74f343ecc749718d258b367b4f
liufa <2587038142@qq.com>
2017-07-03 08:35:53 +0800

    add a repo

---
+++ src/main/java/cn/joylau/code/JoylauMultiGitRepoApplication.java
@@ -0,0 +1,12 @@
+package cn.joylau.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoylauMultiGitRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoylauMultiGitRepoApplication.class, args);
+	}
+}
