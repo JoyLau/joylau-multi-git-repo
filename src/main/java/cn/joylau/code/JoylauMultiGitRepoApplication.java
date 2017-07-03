c7996cfc4deae20d3caab1206a154a3aa3c23072
liufa <2587038142@qq.com>
2017-07-03 08:57:45 +0800

    add a method

--- src/main/java/cn/joylau/code/JoylauMultiGitRepoApplication.java
+++ src/main/java/cn/joylau/code/JoylauMultiGitRepoApplication.java
@@ -1,12 +1,21 @@
 package cn.joylau.code;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JoylauMultiGitRepoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JoylauMultiGitRepoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(JoylauMultiGitRepoApplication ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+		};
 	}
 }
